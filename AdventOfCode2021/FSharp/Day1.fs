5d22886f37d38a98af96ea2d3b41fb311679ba8b
rcurtis <rjcurtis22@gmail.com>
2021-12-01 13:22:01 -0500

    Finished F# version of day 1

--- AdventOfCode2021/FSharp/Day1.fs
+++ AdventOfCode2021/FSharp/Day1.fs
@@ -1,9 +1,78 @@
 ﻿module FSharp.Day1
 
-open Microsoft.VisualStudio.TestTools.UnitTesting
-open Xunit
+open System.IO
+open FSharp.Core
+
+let parseFileInput (path: string) : string = File.ReadAllText(path)
+
+let parseInput (text: string) : int array =
+    text.Split("\r\n")
+    |> Array.map (fun it -> it |> int)
+
+type State =
+    { last: int option
+      total: int }
+    static member empty = { last = None; total = 0 }
+
+
+let part1 (text: string) : int =
+    let input = parseInput text
+
+    let folder (state: State) (current: int) : State =
+        match state.last with
+        | None -> { state with last = Some current }
+        | Some last ->
+            match current > last with
+            | true -> { last = Some current; total = state.total + 1 }
+            | false -> { state with last = Some current }
+
+    input
+    |> Array.fold folder State.empty
+    |> fun it -> it.total
+
+
+let part2 (text: string) : int =
+    let input = parseInput text
+
+    let folder (state: State) (current: int []) : State =
+        let sum = current |> Array.sum
+        
+        match state.last with
+        | None -> { state with last = Some sum }
+        | Some last ->
+            match sum > last with
+            | true -> { last = Some sum; total = state.total + 1 }
+            | false -> { state with last = Some sum }
+
+    input
+    |> Array.windowed 3
+    |> Array.fold folder State.empty
+    |> fun it -> it.total
+
 
 module Test =
+    open Xunit
+
+    let smallInput =
+        @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
     [<Fact>]
-    let ``My Test``() =
-        Assert.True false
+    let ``Part 1 Test`` () =
+        let expected = 7
+        let actual = part1 smallInput
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``Part 2 Test`` () =
+        let expected = 5
+        let actual = part2 smallInput
+        Assert.Equal(expected, actual)
--- AdventOfCode2021/FSharp/Program.fs
+++ AdventOfCode2021/FSharp/Program.fs
@@ -1,4 +1,11 @@
 
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open FSharp
+open System
+
+let day1Input = Day1.parseFileInput "Day1Input.txt"
+let day1Part1 = Day1.part1 day1Input
+Console.WriteLine $"Day 1 Part 1: {day1Part1}"
+
+let day1Part2 = Day1.part2 day1Input
+Console.WriteLine $"Day 1 Part 2: {day1Part2}"
